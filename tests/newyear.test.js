87d4a928b76b2388911ae8190b75255eb00bbfe6
LaycoNi <nicolay.kudryavtsev@gmail.com>
2023-03-20 15:21:59 +0700

    ТЕСТ. Разница дат после прихода Нового Года :D

--- tests/newyear.test.js
+++ tests/newyear.test.js
@@ -21,9 +21,11 @@ for (let i = 1; i <= NUMBER_OF_TESTS; i++) {
     secondDate.month = 1;
     secondDate.year = firstDate.year + 1;
 
-    let dif = Math.abs(
-      firstDate.dateToDays(firstDate.year, firstDate.month, firstDate.day) -
-        secondDate.dateToDays(secondDate.year, secondDate.month, secondDate.day)
+    let date1 = new Date(firstDate.year, firstDate.month - 1, firstDate.day);
+    let date2 = new Date(secondDate.year, secondDate.month - 1, secondDate.day);
+
+    let dif = Math.round(
+      Math.abs(date2.getTime() - date1.getTime()) / (1000 * 3600 * 24)
     );
 
     console.log(
