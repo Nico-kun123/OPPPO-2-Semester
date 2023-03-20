ca2332ea00fca9726abb70c269a20f7224a142b9
LaycoNi <nicolay.kudryavtsev@gmail.com>
2023-03-20 15:16:49 +0700

    Импорт модулей для измерения времени работы

--- main.js
+++ main.js
@@ -5,18 +5,18 @@
 "use strict";
 import * as js from "./modules/days.js";
 import * as ts from "./modules/days.ts";
-import { NUMBER_OF_TESTS } from "./help/constants.js";
+import { NUMBER_OF_DATES } from "./help/constants.js";
 
 // Импорт класса ДАТ
 import { CustomDate } from "./help/class.js";
 
 function timeMeasure(language) {
   let start = performance.now();
-  for (let i = 0; i < NUMBER_OF_TESTS; i++) {
+  for (let i = 0; i < NUMBER_OF_DATES; i++) {
     let firstDate = new CustomDate();
     let secondDate = new CustomDate();
 
-    // Вычислить и вывести результаты на экран 
+    // Вычислить и вывести результаты на экран
     console.log(
       `\nDate 1: ${firstDate.day}-${firstDate.month}-${firstDate.year}`,
       `\nDate 2: ${secondDate.day}-${secondDate.month}-${secondDate.year}\n`,
