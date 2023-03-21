4ebaad3809f80e4130e2e7dcf46b3f6594fccd41
LaycoNi <nicolay.kudryavtsev@gmail.com>
2023-03-21 16:03:04 +0700

    ТЕСТ. Разница дат в днях (Случайные Даты)

--- tests/randomdate.test.js
+++ tests/randomdate.test.js
@@ -12,21 +12,22 @@ import { CustomDate } from "../help/class.js";
 // ТЕСТЫ ДЛЯ АБСОЛЮТНО СЛУЧАЙНЫХ ДАТ
 for (let i = 1; i <= NUMBER_OF_TESTS; i++) {
   test(`Random Date Test №${i}: `, () => {
+    // ТЕСТ: День, месяц и год СЛУЧАЙНЫЕ
     let firstDate = new CustomDate();
     let secondDate = new CustomDate();
 
+    // ~~~
     let date1 = new Date(firstDate.year, firstDate.month - 1, firstDate.day);
     let date2 = new Date(secondDate.year, secondDate.month - 1, secondDate.day);
-
     let dif = Math.round(
       Math.abs(date2.getTime() - date1.getTime()) / (1000 * 3600 * 24)
     );
 
     console.log(
       `RANDOM DATE TEST №${i}
-    Date 1:\t\t${firstDate.day}-${firstDate.month}-${firstDate.year}
-    Date 2:\t\t${secondDate.day}-${secondDate.month}-${secondDate.year}
-    Difference:\t${dif} days! (NOT including end date)`
+        Date 1:\t\t${firstDate.day}-${firstDate.month}-${firstDate.year}
+        Date 2:\t\t${secondDate.day}-${secondDate.month}-${secondDate.year}
+        Difference:\t${dif} days! (NOT including end date)`
     );
 
     expect(js(firstDate, secondDate)).toBe(
