d96ad4a67d42f38eae00a28b54fc5e1c2c5119ac
LaycoNi <nicolay.kudryavtsev@gmail.com>
2023-03-21 16:02:40 +0700

    ТЕСТ. Разница дат после прихода Нового Года :D

--- tests/newyear.test.js
+++ tests/newyear.test.js
@@ -8,31 +8,30 @@ import { NUMBER_OF_TESTS } from "../help/constants.js";
 
 // Импорт класса ДАТ
 import { CustomDate } from "../help/class.js";
+import { randomDay, randomMonth, randomYear } from "../help/random.js";
 
 // ТЕСТЫ ДЛЯ ОСОБЫХ ДАТ (С НОВЫМ ГОДОМ!!!)
 for (let i = 1; i <= NUMBER_OF_TESTS; i++) {
-  test(`Special Test №${i} (Happy New Year!!!): `, () => { 
-    let firstDate = new CustomDate();
-    let secondDate = new CustomDate();
+  test(`Special Test №${i} (Happy New Year!!!): `, () => {
+    // ТЕСТ: День случайный. Месяцы: декабрь (12) и январь (1). Год отличается на 1
+    let year = randomYear();
+    let firstDate = new CustomDate(randomDay(), 12, year, false);
+    let secondDate = new CustomDate(randomDay(), 1, year + 1, false);
+    firstDate.adjustDateIfNeeded();
+    secondDate.adjustDateIfNeeded();
 
-    firstDate.month = 12;
-    firstDate.year = 2021;
-
-    secondDate.month = 1;
-    secondDate.year = firstDate.year + 1;
-
+    // ~~~
     let date1 = new Date(firstDate.year, firstDate.month - 1, firstDate.day);
     let date2 = new Date(secondDate.year, secondDate.month - 1, secondDate.day);
-
     let dif = Math.round(
       Math.abs(date2.getTime() - date1.getTime()) / (1000 * 3600 * 24)
     );
 
     console.log(
       `SPECIAL TEST №${i} (HAPPY NEW YEAR!!!)
-  Date 1:\t\t${firstDate.day}-${firstDate.month}-${firstDate.year}
-  Date 2:\t\t${secondDate.day}-${secondDate.month}-${secondDate.year}
-  Difference:\t${dif} days! (NOT including end date)`
+        Date 1:\t\t${firstDate.day}-${firstDate.month}-${firstDate.year}
+        Date 2:\t\t${secondDate.day}-${secondDate.month}-${secondDate.year}
+        Difference:\t${dif} days! (NOT including end date)`
     );
 
     expect(js(firstDate, secondDate)).toBe(
