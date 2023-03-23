caa1d9bb6ba162412529fb8d7be823db179e94bb
LaycoNi <nicolay.kudryavtsev@gmail.com>
2023-03-23 12:18:20 +0700

    Класс для генерации дат и дальнейшей работы с ними

--- help/class.js
+++ help/class.js
@@ -1,10 +1,21 @@
-import { MIN_DATE, MAX_DATE } from "./constants.js";
+import { MIN_YEAR, MAX_YEAR } from "./constants.js";
 
+/**
+ * Класс, предназначен для работы с датами и содержит методы для генерации случайной даты, проверки правильности даты и т.п.
+ * @class "CustomDate"
+ */
 export class CustomDate {
+  /**
+   * Конструктор класса "CustomDate"
+   * @constructor
+   * @param {number} day день
+   * @param {number} month месяц
+   * @param {number} year год
+   * @param {boolean} random параметр для генерации случайной или конкретной даты. Этот параметр надо передовать только при создании конкретной даты (и он должен быть равен "false"). По умолчанию равен "true"
+   */
   constructor(day = 0, month = 0, year = 0, random = true) {
-    // Генерируем случайный год от MIN_DATE до MAX_DATE
-    // Генерируем случайный месяц от 1 до 12
-    // Генерируем случайный день от 1 до 31
+    // Если random == true, то даты генерируются случайным образом,
+    // иначе дню, месяцу и году присваиваются входные данные у конструктора
     if (random) {
       this.year = this.randYear();
       this.month = this.randMonth();
@@ -18,7 +29,10 @@ export class CustomDate {
     this.adjustDateIfNeeded();
   }
 
-  // Меняет параметры у даты, если дата неправильная
+  /**
+   * Меняет день, месяц и год у даты, если они не корректные
+   * @method
+   */
   adjustDateIfNeeded() {
     if (!this.checkMonth()) {
       this.month = this.randMonth();
@@ -34,7 +48,11 @@ export class CustomDate {
     }
   }
 
-  // Проверка месяца
+  /**
+   * Проверка месяца на корректность
+   * @method
+   * @returns {boolean} true, если месяц правильный, иначе false
+   */
   checkMonth() {
     return (
       this.month <= 12 &&
@@ -44,12 +62,25 @@ export class CustomDate {
     );
   }
 
-  // Проверка года
+  /**
+   * Проверка года на корректность
+   * @method
+   * @returns {boolean} true, если год правильный, иначе false
+   */
   checkYear() {
-    return typeof this.year === "number" && !isNaN(this.year);
+    return (
+      this.year <= MAX_YEAR &&
+      this.year >= MIN_YEAR &&
+      typeof this.year === "number" &&
+      !isNaN(this.year)
+    );
   }
 
-  // Проверка дня
+  /**
+   * Проверка дня на корректность
+   * @method
+   * @returns {boolean} true, если день правильный, иначе false
+   */
   checkDay() {
     return (
       (this.day > 0 && typeof this.day === "number" && !isNaN(this.day)) ||
@@ -57,19 +88,40 @@ export class CustomDate {
     );
   }
 
+  /**
+   * Генерация случайного дня
+   * @method
+   * @returns {number} Случайное число от 1 до 31 включительно
+   */
   randDay() {
     return Math.floor(Math.random() * 31) + 1;
   }
 
+  /**
+   * Генерация случайного месяца
+   * @method
+   * @returns {number} Случайное число от 1 до 12 включительно
+   */
   randMonth() {
     return Math.floor(Math.random() * 12) + 1;
   }
 
+  /**
+   * Генерация случайного года
+   * @method
+   * @returns {number} Случайное число от MIN_YEAR до MAX_YEAR включительно (эти константы находятся в файле "/help/constants.js")
+   */
   randYear() {
-    return Math.floor(Math.random() * (MAX_DATE - MIN_DATE + 1)) + MIN_DATE;
+    return Math.floor(Math.random() * (MAX_YEAR - MIN_YEAR + 1)) + MIN_YEAR;
   }
 
-  // Возвращает кол-во дней для данного месяца
+  /**
+   * Возвращает количество дней в данном месяце данного года
+   * @method
+   * @param {number} year год
+   * @param {number} month месяц
+   * @returns {number} количество дней в данном месяце данного года (с учетом високосности года)
+   */
   getDaysInMonth(year, month) {
     switch (month) {
       case 2:
@@ -84,12 +136,24 @@ export class CustomDate {
     }
   }
 
-  // Високосный ли год или нет?
+  /**
+   * Проверяет, является ли год високосным или нет
+   * @method
+   * @param {number} year год для проверки
+   * @returns {boolean} true, если год високосный, иначе false
+   */
   isLeapYear(year) {
     return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
   }
 
-  // Перевод даты в дни
+  /**
+   * Переводит дату в количество дней (с учетом високосных лет). Необходимо для поиска разницы между датами в днях
+   * @method
+   * @param {number} year год
+   * @param {number} month месяц
+   * @param {number} day день
+   * @returns {number} количество дней для данных параметров даты
+   */
   dateToDays(year, month, day) {
     let days = 0;
     for (let i = 1; i < year; i++) {
