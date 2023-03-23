9c8e52b0b377a9c69f129d0b0b2b7abe479b8756
LaycoNi <nicolay.kudryavtsev@gmail.com>
2023-03-23 12:20:25 +0700

    Импорт модулей для измерения времени работы

--- main.js
+++ main.js
@@ -5,26 +5,31 @@
 "use strict";
 import * as js from "./modules/days.js";
 import * as ts from "./modules/days.ts";
-import { NUMBER_OF_DATES } from "./help/constants.js";
+import { NUMBER_OF_PAIRS } from "./help/constants.js";
 
 // Импорт класса ДАТ
 import { CustomDate } from "./help/class.js";
 
+/**
+ * Генерация пар дат, вычисление разницы м/у ними в днях, вычисление времени работы
+ * @param {any} language модули js и ts, которые в этот файл были импортированы
+ * @returns {string} сколько времени заняло выполнение модулей js и ts
+ */
 function timeMeasure(language) {
   let start = performance.now();
-  for (let i = 0; i < NUMBER_OF_DATES; i++) {
+  for (let i = 0; i < NUMBER_OF_PAIRS; i++) {
     let firstDate = new CustomDate();
     let secondDate = new CustomDate();
 
-    // Вычислить и вывести результаты на экран
-    console.log(
-      `\nDate 1: ${firstDate.day}-${firstDate.month}-${firstDate.year}`,
-      `\nDate 2: ${secondDate.day}-${secondDate.month}-${secondDate.year}\n`,
-      language.count_days(firstDate, secondDate)
-    );
+    // Вычислить и вывести результаты в консоль
+    // console.log(
+    //   `\nDate 1: ${firstDate.day}-${firstDate.month}-${firstDate.year}`,
+    //   `\nDate 2: ${secondDate.day}-${secondDate.month}-${secondDate.year}\n`,
+    //   language.count_days(firstDate, secondDate)
+    // );
 
     // Просто Вычислить
-    // language.count_days(firstDate, secondDate);
+    language.count_days(firstDate, secondDate);
   }
   let end = performance.now();
 
